6f391bb61108107fbfa0e0aba8fb1668f86e3cad
fernandocandidodesiqueirajunior <fernando.candido.siqueira.junior@gmail.com>
2021-11-06 22:25:50 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+import com.devsuperior.dsvendas.entities.Sale;
+
+
+public interface SaleRepository extends JpaRepositoryImplementation<Sale, Long>{
+	
+
+}
